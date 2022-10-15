9f9f44b484de797ef47eff3916be97b5ea37b811
noppefoxwolf <noppelabs@gmail.com>
2022-10-16 02:32:50 +0900

    Update CPlaydate-Bridging-Header.h

--- Headers/CPlaydate-Bridging-Header.h
+++ Headers/CPlaydate-Bridging-Header.h
@@ -1,10 +1,10 @@
 #define TARGET_EXTENSION 1
 #include "panic/pd_api.h"
 
-void logToConsole(playdate_sys *system, const char* fmt) {
+void logToConsole(playdate_sys system, const char* fmt) {
   system->logToConsole(fmt);
 }
 
-void errorToConsole(playdate_sys *system, const char* fmt) {
+void errorToConsole(playdate_sys system, const char* fmt) {
   system->error(fmt);
 }
