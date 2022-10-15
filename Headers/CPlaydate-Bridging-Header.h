dbbff0a0dd972162a91987c14d6142384feaaaf6
noppefoxwolf <noppelabs@gmail.com>
2022-10-16 02:34:02 +0900

    Update CPlaydate-Bridging-Header.h

--- Headers/CPlaydate-Bridging-Header.h
+++ Headers/CPlaydate-Bridging-Header.h
@@ -2,9 +2,9 @@
 #include "panic/pd_api.h"
 
 void logToConsole(playdate_sys system, const char* fmt) {
-  system->logToConsole(fmt);
+  system.logToConsole(fmt);
 }
 
 void errorToConsole(playdate_sys system, const char* fmt) {
-  system->error(fmt);
+  system.error(fmt);
 }
